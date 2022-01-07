9a44c798b239b9d67c24b6d331e7d9b1a7d72128
Strada Fabio <fabio.strada@allitude.it>
2022-01-07 11:20:57 +0100

    print configurations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, LOCALE_ID } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
 import { AppConfig } from './core/config/app-config';
 
 @Component({
@@ -9,9 +10,10 @@ import { AppConfig } from './core/config/app-config';
 export class AppComponent {
     
   constructor(private appConfig: AppConfig,
-             @Inject(LOCALE_ID) locale: string) {
-      console.log('Configuration',appConfig);
-      console.log('Locale', locale);
+             @Inject(LOCALE_ID) private locale: string) {
+    console.log('environment.production', environment.production, environment.appVersion);    
+    console.log('Configuration',appConfig);
+    console.log('Locale', locale);    
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  appVersion: '0.0.1'
+  appVersion: '0.0.0'
 };
