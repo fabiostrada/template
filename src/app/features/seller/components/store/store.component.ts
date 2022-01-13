d63facb378e086540242292944ef9ac7997948a7
Strada Fabio <fabio.strada@allitude.it>
2022-01-13 14:51:33 +0100

    fix on store

--- src/app/features/seller/components/store/store.component.ts
+++ src/app/features/seller/components/store/store.component.ts
@@ -6,12 +6,6 @@ import { ArticleService } from 'src/app/core/services/article.service';
   templateUrl: './store.component.html',
   styleUrls: ['./store.component.scss']
 })
-export class StoreComponent implements OnInit {
-
-  constructor(private articleService: ArticleService) { }
-
-  ngOnInit(): void {
-      this.articleService.allArticles().subscribe(console.log);
-  }
+export class StoreComponent {
 
 }
